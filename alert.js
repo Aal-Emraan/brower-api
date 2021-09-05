35cabe56c61f16cca31f00fbfd12773a959346b6
Aal Emraan <developer.aalemraan@gmail.com>
2021-09-05 12:37:30 +0600

    alerts learned

--- alert.js
+++ alert.js
@@ -4,5 +4,19 @@ const show = () => {
 
 const hello = () => {
     const res = confirm('are you confirm?');
-    console.log(res)
+    if(res === true){
+        let money = prompt('please provide money.');
+        alert('you have given '+ money + ' taka')
+    }else{
+        alert('ok thanks...')
+        open('https://google.com');
+        close('https://google.com')
+    }
+}
+
+const info = () => {
+    const name = prompt('what is your name?');
+    const address = prompt('what is your address?');
+    const phone = prompt('what is your phone number?')
+    alert(`Your name is ${name}. Your address is ${address} and your phone number is ${phone}.`)
 }
