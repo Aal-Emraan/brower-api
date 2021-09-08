e69185222ba4ff370aca9e6bc55c390e1380f8ad
Aal Emraan <developer.aalemraan@gmail.com>
2021-09-08 12:15:55 +0600

    cart practiced

--- shopping.js
+++ shopping.js
@@ -1,14 +1,19 @@
 const addItem = () => {
     const input = document.getElementById('input-field');
-    if(input.value === ''){
+    const value = input.value;
+    if(!value){
         return;
     }
+    add(value);
+    input.value = '';
+}
+
+const add = (value) => {
     const item = document.createElement('li');
-    item.innerText = input.value;
+    item.innerText = value;
     const list = document.getElementById('list');
     list.appendChild(item);
-    setItem(input.value);
-    input.value = '';
+    setItem(value);
 }
 
 const getCart = () => {
@@ -24,7 +29,33 @@ const getCart = () => {
 
 const setItem = name => {
     const cart = getCart();
+    const cartcheck = JSON.stringify(cart);
     cart[name] = 1;
     const stringified = JSON.stringify(cart);
     localStorage.setItem('cart', stringified);
+}
+
+const prevCart = () => {
+    const cart = getCart();
+    for(const item in cart){
+        add(item);
+    }
+/*     const prevCart = JSON.parse(localStorage.getItem('cart'));
+    console.log(prevCart);
+    if(prevCart){
+        for(c in prevCart){
+            const list = document.getElementById('list');
+            const li = document.createElement('li');
+            li.innerText = c;
+            list.appendChild(li);
+        }
+    } */
+}
+prevCart()
+
+const placeOrder = () => {
+    const ul = document.getElementById('list');
+    ul.innerHTML = '';
+    localStorage.removeItem('cart');
+    
 }
